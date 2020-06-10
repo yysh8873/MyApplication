d9c81b24439f85d44d5b254d9689d50add6e5241
WooJooYoung <wnduddk1119@users.noreply.github.com>
2020-06-10 20:33:58 +0900

    주영 주문 레이아웃 수정

--- app/src/main/java/com/example/myapplication/Order.java
+++ app/src/main/java/com/example/myapplication/Order.java
@@ -20,6 +20,10 @@ public class Order extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     private RecyclerView recyclerview;
     private RecyclerAdapter adapter;
+    OrderListArray ol;
+    int rice;
+    int juice;
+    int soju;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -77,80 +81,166 @@ public class Order extends AppCompatActivity {
             }
         });
 
-        // 리스트 연결
-        init();
-
-        getData();
-
-        final ListView listView = findViewById(R.id.listView);
-        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                new String[]{"공기밥", "소주"}));
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        Button sideminus1 = (Button) findViewById(R.id.sideminus1);
+        sideminus1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        AlertDialog.Builder dlg1 = new AlertDialog.Builder(Order.this);
-                        dlg1.setTitle("장바구니 삭제");
-                        dlg1.setMessage("장바구니에서 " + parent.getItemAtPosition(position) + "를 빼시겠습니까?");
-                        dlg1.setIcon(R.drawable.delete);
-                        dlg1.setPositiveButton("확인",null);
-                        dlg1.show();
-                    }
-                });
+            public void onClick(View view) {
+                TextView sidecount1 = (TextView) findViewById(R.id.sidecount1);
+                String count1 = sidecount1.getText().toString();
+                rice = Integer.parseInt(count1);
+                if (rice > 0) rice--;
+                sidecount1.setText(Integer.toString(rice));
             }
         });
 
+        Button sideplus1 = (Button) findViewById(R.id.sideplus1);
+        sideplus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView sidecount1 = (TextView) findViewById(R.id.sidecount1);
+                String count1 = sidecount1.getText().toString();
+                rice = Integer.parseInt(count1);
+                rice++;
+                sidecount1.setText(Integer.toString(rice));
+            }
+        });
+
+        Button sideminus2 = (Button) findViewById(R.id.sideminus2);
+        sideminus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView sidecount2 = (TextView) findViewById(R.id.sidecount2);
+                String count2 = sidecount2.getText().toString();
+                juice = Integer.parseInt(count2);
+                if (juice > 0) juice--;
+                sidecount2.setText(Integer.toString(juice));
+            }
+        });
+
+        Button sideplus2 = (Button) findViewById(R.id.sideplus2);
+        sideplus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView sidecount2 = (TextView) findViewById(R.id.sidecount2);
+                String count2 = sidecount2.getText().toString();
+                juice = Integer.parseInt(count2);
+                juice++;
+                sidecount2.setText(Integer.toString(juice));
+            }
+        });
+
+        Button sideminus3 = (Button) findViewById(R.id.sideminus3);
+        sideminus3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView sidecount3 = (TextView) findViewById(R.id.sidecount3);
+                String count3 = sidecount3.getText().toString();
+                soju = Integer.parseInt(count3);
+                if (soju > 0) soju--;
+                sidecount3.setText(Integer.toString(soju));
+            }
+        });
+
+        Button sideplus3 = (Button) findViewById(R.id.sideplus3);
+        sideplus3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView sidecount3 = (TextView) findViewById(R.id.sidecount3);
+                String count3 = sidecount3.getText().toString();
+                soju = Integer.parseInt(count3);
+                soju++;
+                sidecount3.setText(Integer.toString(soju));
+            }
+        });
+
+        // 리스트 연결
+//        init();
+//
+//        getData();
+        String[] sidemenu = new String[3];
+        sidemenu[0] = "공기밥 "+rice+"개";
+        sidemenu[1] = "탄산음료(1.25L) "+juice+"개";
+        sidemenu[2] = "소주 "+soju+"개";
+
+//        final ListView listView = findViewById(R.id.listView);
+//        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+//                new String[]{"공기밥 "+rice+"개", "탄산음료(1.25L) "+juice+"개", "소주 "+soju+"개"}));
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                    @Override
+//                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                        AlertDialog.Builder dlg1 = new AlertDialog.Builder(Order.this);
+//                        dlg1.setTitle("장바구니 삭제");
+//                        dlg1.setMessage("장바구니에서 " + parent.getItemAtPosition(position) + "를 빼시겠습니까?");
+//                        dlg1.setIcon(R.drawable.delete);
+//                        dlg1.setPositiveButton("확인",null);
+//                        dlg1.show();
+//                    }
+//                });
+//            }
+//        });
+
 
     }
 
-    private void init() {
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+//    private void init() {
+//        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+//
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//
+//        adapter = new RecyclerAdapter();
+//        recyclerView.setAdapter(adapter);
+//    }
+//
+//    private void getData() {
+//        // 임의의 데이터입니다.
+//        List<String> listTitle = Arrays.asList("공기밥", "탄산음료(1.25L)", "소주");
+//        List<String> listContent = Arrays.asList(
+//                "1000원",
+//                "2000원",
+//                "4000원"
+//        );
+//
+//        for (int i = 0; i < listTitle.size(); i++) {
+//            // 각 List의 값들을 data 객체에 set 해줍니다.
+//            Data data = new Data();
+//            data.setTitle(listTitle.get(i));
+//            data.setContent(listContent.get(i));
+//            data.setBtn("선택");
+//            data.setDlgTitle("장바구니 담기");
+//            data.setDlgMsg("선택한 항목을 장바구니에 담으시겠습니까?");
+//            final int finalI = i;
+//            data.setDlgPB(new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    Toast.makeText(Order.this, "장바구니 담았습니다",
+//                            Toast.LENGTH_SHORT).show();
+//                    if(finalI == 0) rice++;
+//                    else if(finalI == 1) juice++;
+//                    else if(finalI == 2) soju++;
+//                }
+//            });
+//
+//            // 각 값이 들어간 data를 adapter에 추가합니다.
+//            adapter.addItem(data);
+//        }
+//
+//        // adapter의 값이 변경되었다는 것을 알려줍니다.
+//        adapter.notifyDataSetChanged();
+//
+//    }
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        adapter = new RecyclerAdapter();
-        recyclerView.setAdapter(adapter);
+    public int[] getItemCount() {
+        int[] itemCount = new int[3];
+        itemCount[0] = rice;
+        itemCount[1] = juice;
+        itemCount[2] = soju;
+        return itemCount;
     }
-
-    private void getData() {
-        // 임의의 데이터입니다.
-        List<String> listTitle = Arrays.asList("공기밥", "탄산음료(1.25L), 소주");
-        List<String> listContent = Arrays.asList(
-                "1000원",
-                "2000원",
-                "4000원"
-        );
-
-        for (int i = 0; i < listTitle.size(); i++) {
-            // 각 List의 값들을 data 객체에 set 해줍니다.
-            Data data = new Data();
-            data.setTitle(listTitle.get(i));
-            data.setContent(listContent.get(i));
-            data.setBtn("선택");
-            data.setDlgTitle("장바구니 담기");
-            data.setDlgMsg("선택한 항목을 장바구니에 담으시겠습니까?");
-            data.setDlgPB(new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Toast.makeText(Order.this, "장바구니 담았습니다",
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            // 각 값이 들어간 data를 adapter에 추가합니다.
-            adapter.addItem(data);
-        }
-
-        // adapter의 값이 변경되었다는 것을 알려줍니다.
-        adapter.notifyDataSetChanged();
-
-    }
-
-
 
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
