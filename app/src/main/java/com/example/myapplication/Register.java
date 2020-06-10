9bf4be295025c8b2147cef98e15c2d9e4fb3dd3b
yeon-06 <rnjstldus2@hs.ac.kr>
2020-06-10 14:43:57 +0900

    siyeon add register function

--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -11,6 +11,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -24,6 +25,16 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.app.ProgressDialog;
 
 
 public class Register extends AppCompatActivity {
@@ -31,6 +42,15 @@ public class Register extends AppCompatActivity {
     private WebView webView;
     private TextView txt_address;
     private Handler handler;
+
+    private static String IP_ADDRESS = "13.125.45.205";
+    private static String TAG = "MyApplication";
+
+    private EditText mEditTextId;
+    private EditText mEditTextPw;
+    private EditText mEditTextName;
+    private EditText mEditTextPhone;
+    private EditText mEditTextAddr;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,18 +63,14 @@ public class Register extends AppCompatActivity {
 
         actionBar.setDisplayHomeAsUpEnabled(true);   //업버튼 <- 만들기
 
-        final TextView status = (TextView) findViewById(R.id.status);
-        // 위치에 대한 권한 요구
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                MY_PERMISSIONS_REQUEST_LOCATION);
-        // 위치 관리자에 대한 참조값
-        LocationManager locationManager = (LocationManager)
-                this.getSystemService(Context.LOCATION_SERVICE);
-
+        mEditTextId = (EditText) findViewById(R.id.edit_ID);
+        mEditTextPw = (EditText) findViewById(R.id.edit_PW);
+        mEditTextName = (EditText) findViewById(R.id.edit_Name);
+        mEditTextPhone = (EditText) findViewById(R.id.edit_Phone);
+        mEditTextAddr = (EditText) findViewById(R.id.edit_AD);
 
         Button btn_register = (Button) findViewById(R.id.btn_register);
-
+/*
         final EditText edit_ID = (EditText) findViewById(R.id.edit_ID);
         String str_ID = edit_ID.getText().toString();
 
@@ -70,48 +86,103 @@ public class Register extends AppCompatActivity {
         edit_Phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
 
         final EditText edit_AD = (EditText) findViewById(R.id.edit_AD);
-        edit_AD.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SearchAddress.class);
-                startActivity(intent);
-            }
-        });
+ */
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String str_ID = mEditTextId.getText().toString();
+                String str_PW = mEditTextPw.getText().toString();
+                String str_Name = mEditTextName.getText().toString();
+                String str_Phone = mEditTextPhone.getText().toString();
+                String str_Adr = mEditTextAddr.getText().toString();
+
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+
+                InsertData task = new InsertData();
+
+                task.execute("http://"+IP_ADDRESS+"/register.php",str_ID, str_PW,str_Name,str_Phone,str_Adr);
+                mEditTextId.setText("");
+                mEditTextPw.setText("");
+                mEditTextName.setText("");
+                mEditTextPhone.setText("");
+                mEditTextAddr.setText("");
             }
         });
+    }
 
-        LocationListener locationListener = new LocationListener() {
-            // 새로운 위치가 발견되면 호출
-            public void onLocationChanged(Location location) {
-                status.setText("위도; " + location.getLatitude()
-                        + "\n경도:" + location.getLongitude()
-                        + "\n고도:" + location.getAltitude());
-            }
-            // 위치가 업데이트되면 호출되는 리스너 정의
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-            public void onProviderEnabled(String provider) {}
-            public void onProviderDisabled(String provider) {}
-        };
 
-        if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this,
-                        android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(Register.this, "First enable LOCATION ACCESS in settings.",
-                    Toast.LENGTH_LONG).show();
-            return;
+    class InsertData extends AsyncTask<String, Void, String>{
+        /*
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(Register.this,
+                    "Please Wait", null, true, true);
         }
-        // 위치를 업데이트 받기 위해 리스너를 위치 관리자에 등록
-        locationManager.requestLocationUpdates(
-                LocationManager. GPS_PROVIDER , 0, 0, locationListener);
+        */
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String name = (String)params[1];
+            String country = (String)params[2];
+
+            String serverURL = (String)params[0];
+            String postParameters = "name=" + name + "&country=" + country;
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                return sb.toString();
+
+            } catch (Exception e) {
+                Log.d(TAG, "InsertData: Error ", e);
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
     }
 
     @Override
