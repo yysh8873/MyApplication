00d35c210e44d730e7bd93df129f2e60ce5cbf60
yeon-06 <rnjstldus2@hs.ac.kr>
2020-06-20 19:37:38 +0900

    Merge branch 'master' of C:\Users\rnjst\AndroidStudioProjects\MyApplication with conflicts.

--- app/src/main/java/com/example/myapplication/HomeList.java
+++ app/src/main/java/com/example/myapplication/HomeList.java
@@ -1,35 +1,20 @@
 package com.example.myapplication;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-
 
 public class HomeList extends Fragment{
     ViewGroup viewGroup;
@@ -37,50 +22,10 @@ public class HomeList extends Fragment{
     private RecyclerAdapter adapter;
     private Object HomeList;
 
-    String serverURL = "http://13.125.45.205/orderlist.php";
-    private static String IP_ADDRESS = "13.125.45.205";
-    private static String TAG = "myapplication";
-
-    private static final String TAG_JSON="webnautes";
-    private static final String TAG_OID = "oid";
-    private static final String TAG_TDATE = "tdate";
-    private static final String TAG_MENU = "menu";
-    private static final String TAG_UID ="uid";
-    private static final String TAG_PRICE ="price";
-    private static final String TAG_ADDR ="addr";
-    private static final String TAG_PHONE ="phone";
-    private static final String TAG_DELCON ="delcon";
-
-    private TextView mTextViewResult;
-    ArrayList<HashMap<String, String>> mArrayList;
-    ListView mListViewList;
-    EditText mEditTextSearchKeyword1, mEditTextSearchKeyword2;
-    String mJsonString;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.home_list,container,false);
-
-        mTextViewResult = (TextView)viewGroup.findViewById(R.id.textView_main_result);
-        mListViewList = (ListView) viewGroup.findViewById(R.id.listView_main_list);
-        mEditTextSearchKeyword1 = (EditText) viewGroup.findViewById(R.id.editText_main_searchKeyword1);
-        System.out.println("로그" + mEditTextSearchKeyword1.getText().toString());
-
-        Button button_search = (Button) viewGroup.findViewById(R.id.button_main_search);
-        button_search.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                mArrayList.clear();
-
-                GetData task = new GetData();
-                task.execute(mEditTextSearchKeyword1.getText().toString());
-
-            }
-        });
-
-
-        mArrayList = new ArrayList<>();
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.home_list,container,false); 
 
         // 리스트 연결
         init();
@@ -88,169 +33,7 @@ public class HomeList extends Fragment{
         getData();
 
         return viewGroup;
-
     }
-
-    class GetData extends AsyncTask<String, Void, String> {
-        ProgressDialog progressDialog;
-        String errorString = null;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            progressDialog = ProgressDialog.show((Context) HomeList,
-                    "Please Wait", null, true, true);
-        }
-
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            progressDialog.dismiss();
-            mTextViewResult.setText("sival");
-            Log.d(TAG, "response - " + result);
-
-            if (result == null){
-
-                mTextViewResult.setText(errorString);
-            }
-            else {
-
-                mJsonString = result;
-                showResult();
-            }
-        }
-
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            String searchKeyword1 = params[0];
-            String searchKeyword2 = params[1];
-
-            String serverURL = "http://13.125.45.205/orderlist.php";
-            String postParameters = "uid=" + searchKeyword1;
-
-
-            try {
-
-                URL url = new URL(serverURL);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-
-                httpURLConnection.setReadTimeout(5000);
-                httpURLConnection.setConnectTimeout(5000);
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoInput(true);
-                httpURLConnection.connect();
-
-
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                outputStream.write(postParameters.getBytes("UTF-8"));
-                outputStream.flush();
-                outputStream.close();
-
-
-                int responseStatusCode = httpURLConnection.getResponseCode();
-                Log.d(TAG, "response code - " + responseStatusCode);
-
-                InputStream inputStream;
-                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
-                    inputStream = httpURLConnection.getInputStream();
-                }
-                else{
-                    inputStream = httpURLConnection.getErrorStream();
-                }
-
-
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                StringBuilder sb = new StringBuilder();
-                String line;
-
-                while((line = bufferedReader.readLine()) != null){
-                    sb.append(line);
-                }
-
-
-                bufferedReader.close();
-
-
-                return sb.toString().trim();
-
-
-            } catch (Exception e) {
-
-                Log.d(TAG, "InsertData: Error ", e);
-                errorString = e.toString();
-
-                return null;
-            }
-
-        }
-    }
-
-
-    private void showResult(){
-        try {
-            JSONObject jsonObject = new JSONObject(mJsonString);
-            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
-
-            for(int i=0;i<jsonArray.length();i++){
-
-                JSONObject item = jsonArray.getJSONObject(i);
-
-                /*    private static final String TAG_OID = "oid";
-                      private static final String TAG_TDATE = "tdate";
-                      private static final String TAG_MENU = "menu";
-                      private static final String TAG_UID ="uid";
-                      private static final String TAG_PRICE ="price";
-                      private static final String TAG_ADDR ="addr";
-                      private static final String TAG_PHONE ="phone";
-                      private static final String TAG_DELCON ="delcon";*/
-
-                String oid = item.getString(TAG_OID);
-                String tdate = item.getString(TAG_TDATE);
-                String menu = item.getString(TAG_MENU);
-                String uid = item.getString(TAG_UID);
-                String price = item.getString(TAG_PRICE);
-                String addr = item.getString(TAG_ADDR);
-                String phone = item.getString(TAG_PHONE);
-                String delcon = item.getString(TAG_DELCON);
-
-                HashMap<String,String> hashMap = new HashMap<>();
-
-                hashMap.put(TAG_OID, oid);
-                hashMap.put(TAG_TDATE, tdate);
-                hashMap.put(TAG_MENU, menu);
-                hashMap.put(TAG_UID, uid);
-                hashMap.put(TAG_PRICE, price);
-                hashMap.put(TAG_ADDR, addr);
-                hashMap.put(TAG_PHONE, phone);
-                hashMap.put(TAG_DELCON, delcon);
-
-                mArrayList.add(hashMap);
-            }
-
-            ListAdapter adapter = new SimpleAdapter(
-                    (Context) HomeList, mArrayList, R.layout.item_list,
-                    new String[]{TAG_OID,TAG_TDATE, TAG_UID},
-                    new int[]{R.id.textView_list_id, R.id.textView_list_name, R.id.textView_list_address}
-            );
-
-            mListViewList.setAdapter(adapter);
-
-        } catch (JSONException e) {
-
-            Log.d(TAG, "showResult : ", e);
-        }
-
-    }
-
-
     
     private void init() {
         RecyclerView recyclerView = viewGroup.findViewById(R.id.recyclerView);
@@ -263,7 +46,6 @@ public class HomeList extends Fragment{
     }
 
     private void getData() {
-
         // 임의의 데이터입니다.
         List<String> listTitle = Arrays.asList("안동찜닭", "치즈순살찜닭", "고추장찜닭");
         List<String> listContent = Arrays.asList(
--- app/src/main/java/com/example/myapplication/Order.java
+++ app/src/main/java/com/example/myapplication/Order.java
@@ -20,9 +20,10 @@ public class Order extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     private RecyclerView recyclerview;
     private RecyclerAdapter adapter;
-    private int rice = 0;
-    private int juice = 0;
-    private int soju = 0;
+    //OrderListArray ol;
+    int rice;
+    int juice;
+    int soju;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,51 @@ public class Order extends AppCompatActivity {
         actionBar.setTitle("한신이닭");  //액션바 제목설정
 
         actionBar.setDisplayHomeAsUpEnabled(true);   //업버튼 <- 만들기
+
+        Button btn_menu1 = (Button) findViewById(R.id.btn_menu1);
+        btn_menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Order_menu1.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn_menu2 = (Button) findViewById(R.id.btn_menu2);
+        btn_menu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Order_menu2.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn_menu3 = (Button) findViewById(R.id.btn_menu3);
+        btn_menu3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Order_menu3.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn_menu4 = (Button) findViewById(R.id.btn_menu4);
+        btn_menu4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Order_menu4.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn_menu5 = (Button) findViewById(R.id.btn_menu5);
+        btn_menu5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Order_menu5.class);
+                startActivity(intent);
+            }
+        });
 
         Button sideminus1 = (Button) findViewById(R.id.sideminus1);
         sideminus1.setOnClickListener(new View.OnClickListener() {
@@ -107,67 +153,93 @@ public class Order extends AppCompatActivity {
             }
         });
 
-        Button btn_menu1 = (Button) findViewById(R.id.btn_menu1);
-        btn_menu1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Order_menu1.class);
-                intent.putExtra("rice", rice);
-                intent.putExtra("juice", juice);
-                intent.putExtra("soju", soju);
-                startActivity(intent);
-            }
-        });
+        // 리스트 연결
+//        init();
+//
+//        getData();
+        String[] sidemenu = new String[3];
+        sidemenu[0] = "공기밥 "+rice+"개";
+        sidemenu[1] = "탄산음료(1.25L) "+juice+"개";
+        sidemenu[2] = "소주 "+soju+"개";
 
-        Button btn_menu2 = (Button) findViewById(R.id.btn_menu2);
-        btn_menu2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Order_menu2.class);
-                intent.putExtra("rice", rice);
-                intent.putExtra("juice", juice);
-                intent.putExtra("soju", soju);
-                startActivity(intent);
-            }
-        });
-
-        Button btn_menu3 = (Button) findViewById(R.id.btn_menu3);
-        btn_menu3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Order_menu3.class);
-                intent.putExtra("rice", rice);
-                intent.putExtra("juice", juice);
-                intent.putExtra("soju", soju);
-                startActivity(intent);
-            }
-        });
-
-        Button btn_menu4 = (Button) findViewById(R.id.btn_menu4);
-        btn_menu4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Order_menu4.class);
-                intent.putExtra("rice", rice);
-                intent.putExtra("juice", juice);
-                intent.putExtra("soju", soju);
-                startActivity(intent);
-            }
-        });
-
-        Button btn_menu5 = (Button) findViewById(R.id.btn_menu5);
-        btn_menu5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Order_menu5.class);
-                intent.putExtra("rice", rice);
-                intent.putExtra("juice", juice);
-                intent.putExtra("soju", soju);
-                startActivity(intent);
-            }
-        });
+//        final ListView listView = findViewById(R.id.listView);
+//        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+//                new String[]{"공기밥 "+rice+"개", "탄산음료(1.25L) "+juice+"개", "소주 "+soju+"개"}));
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                    @Override
+//                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                        AlertDialog.Builder dlg1 = new AlertDialog.Builder(Order.this);
+//                        dlg1.setTitle("장바구니 삭제");
+//                        dlg1.setMessage("장바구니에서 " + parent.getItemAtPosition(position) + "를 빼시겠습니까?");
+//                        dlg1.setIcon(R.drawable.delete);
+//                        dlg1.setPositiveButton("확인",null);
+//                        dlg1.show();
+//                    }
+//                });
+//            }
+//        });
 
 
+    }
+
+//    private void init() {
+//        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+//
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//
+//        adapter = new RecyclerAdapter();
+//        recyclerView.setAdapter(adapter);
+//    }
+//
+//    private void getData() {
+//        // 임의의 데이터입니다.
+//        List<String> listTitle = Arrays.asList("공기밥", "탄산음료(1.25L)", "소주");
+//        List<String> listContent = Arrays.asList(
+//                "1000원",
+//                "2000원",
+//                "4000원"
+//        );
+//
+//        for (int i = 0; i < listTitle.size(); i++) {
+//            // 각 List의 값들을 data 객체에 set 해줍니다.
+//            Data data = new Data();
+//            data.setTitle(listTitle.get(i));
+//            data.setContent(listContent.get(i));
+//            data.setBtn("선택");
+//            data.setDlgTitle("장바구니 담기");
+//            data.setDlgMsg("선택한 항목을 장바구니에 담으시겠습니까?");
+//            final int finalI = i;
+//            data.setDlgPB(new DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    Toast.makeText(Order.this, "장바구니 담았습니다",
+//                            Toast.LENGTH_SHORT).show();
+//                    if(finalI == 0) rice++;
+//                    else if(finalI == 1) juice++;
+//                    else if(finalI == 2) soju++;
+//                }
+//            });
+//
+//            // 각 값이 들어간 data를 adapter에 추가합니다.
+//            adapter.addItem(data);
+//        }
+//
+//        // adapter의 값이 변경되었다는 것을 알려줍니다.
+//        adapter.notifyDataSetChanged();
+//
+//    }
+
+    public int[] getItemCount() {
+        int[] itemCount = new int[3];
+        itemCount[0] = rice;
+        itemCount[1] = juice;
+        itemCount[2] = soju;
+        return itemCount;
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
